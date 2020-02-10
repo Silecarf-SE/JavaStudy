6a456fc563adc1b19be3080b316f9851f46ad206
Silecarf-SE <silecarf@gmail.com>
2020-02-10 16:51:01 +0900

    java Study 1st

---
+++ src/org/javatutorials/number/Number.java
@@ -0,0 +1,12 @@
+package org.javatutorials.number;
+
+public class Number {
+
+    public static void main(String[] args) {
+
+        System.out.println("A\"B\"CD" + "EFGH");
+        // write your code here
+
+        //vim 계열인가 i로 인서트하네
+    }
+}
