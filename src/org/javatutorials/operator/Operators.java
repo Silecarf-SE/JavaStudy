0645ef2fad36c0a2ee397239dce5e194253eaec0
Silecarf-SE <silecarf@gmail.com>
2020-02-11 11:37:25 +0900

    java Study 2st

---
+++ src/org/javatutorials/operator/Operators.java
@@ -0,0 +1,37 @@
+package org.javatutorials.operator;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println( 1==2);
+        System.out.println( 1== 1);
+        System.out.println("one" != "two");
+        System.out.println("one" == "one");
+        int a=0;
+        int b=3;
+        if(a==b)
+            System.out.println(true);
+        else
+            System.out.println(false);
+        /*
+        String id = args[0];
+        if(id.equals("egoing")){
+            System.out.println("GOGO");
+        }else{
+            System.out.println("Stop");
+        }*/
+        switch (4){
+            case 1:
+                System.out.println(1);
+                break;
+            case 2:
+                System.out.println(2);
+                break;
+            case 3:
+                System.out.println(3);
+                break;
+            default:
+                System.out.println("Default");
+        }
+
+    }
+}
