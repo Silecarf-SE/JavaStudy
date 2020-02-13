77d199231a30af801c2a50fe75d13006daa3b9e1
Silecarf-SE <silecarf@gmail.com>
2020-02-13 12:52:04 +0900

    java Study 4st

--- src/org/javatutorials/io/inputoutput.java
+++ src/org/javatutorials/io/inputoutput.java
@@ -1,10 +1,32 @@
 package org.javatutorials.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
 public class inputoutput {
     public static void main(String[] args){
         for (String e:args) {
             System.out.println(e);
         }
+        try {
+            File file = new File("out.txt");
+            Scanner sc = new Scanner(file);
+
+        /*
+        int i = sc.nextInt();
+        double j = sc.nextDouble();
+        System.out.println(i*10 + " " + j*10);
+        */
+            while (sc.hasNextInt()) {
+                System.out.println(sc.nextInt() * 10);
+            }
+            sc.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
 
     }
 }
