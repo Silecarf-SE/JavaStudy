d19c98f105b6ce752c9148a310937a072e7ae7f1
Silecarf-SE <silecarf@gmail.com>
2020-02-13 12:39:35 +0900

    java Study 4st

--- src/org/javatutorials/Method/Methods.java
+++ src/org/javatutorials/Method/Methods.java
@@ -1,7 +1,42 @@
 package org.javatutorials.Method;
 
-public class Methods {
-    public static void main(String[] args){
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Methods {
+
+    public static void main(String[] args){
+        int k=0;
+
+        String rs = "";
+        rs += numbering(3);
+        rs += numbering(3,4);
+        System.out.println("Number " + rs);
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+            out.write(rs);
+            out.close();
+
+        }catch (IOException e){
+
+        }
+        String[] returnString = getMembers();
+        System.out.println(returnString[0] + returnString[1] + returnString[2]);
+    }
+    public static int numbering(int init, int add) {
+        int s = init;
+        s += add;
+        return s;
+    }
+    public static int numbering(int init){
+        int s = init;
+        s += init;
+        return s;
+    }
+    public static String[] getMembers(){
+        String[] members = {"na", "nu", "uri"};
+        return members;
     }
 }
---
+++ src/org/javatutorials/io/inputoutput.java
@@ -0,0 +1,10 @@
+package org.javatutorials.io;
+
+public class inputoutput {
+    public static void main(String[] args){
+        for (String e:args) {
+            System.out.println(e);
+        }
+
+    }
+}
